1911189813f782be18eeda3448006ef370a7992f
purnomo yusgiantoro <pyxvin124@gmail.com>
2026-01-06 10:39:42 +0700

    perabaiki ca rules

--- backend/app/rules.py
+++ backend/app/rules.py
@@ -3,30 +3,30 @@ RULES = [
         "kode": "K01",
         "hasil": "Sangat Mentah",
         "fakta": ["R01", "R02", "R05", "R07", "R010", "R013"],
-        "cf": 0.9
+        "cf": 1
     },
     {
         "kode": "K02",
         "hasil": "Mentah",
         "fakta": ["R01", "R02", "R05", "R08", "R010", "R013"],
-        "cf": 0.8
+        "cf": 1
     },
     {
         "kode": "K03",
         "hasil": "Matang",
         "fakta": ["R01", "R03", "R06", "R07", "R011", "R013"],
-        "cf": 0.95
+        "cf": 1
     },
     {
         "kode": "K04",
         "hasil": "Sangat Matang",
         "fakta": ["R01", "R03", "R06", "R08", "R011", "R013"],
-        "cf": 0.85
+        "cf": 1
     },
     {
         "kode": "K05",
         "hasil": "Busuk",
         "fakta": ["R01", "R03", "R06", "R09", "R012", "R014"],
-        "cf": 0.99
+        "cf": 1
     },
 ]   
